55e2736ae49a780a8252e94e613305bae3f97cea
djankeee13 <djani-b@live.com>
2022-05-10 16:17:58 +0200

    1
    1

---
+++ src/components/Food.jsx
@@ -0,0 +1,14 @@
+import "./Food.css"
+
+const Food = (props) => {
+    return (  
+        <div style={{ background: `url('${props.bgImg}') no-repeat center center/cover` }} className='food'>
+        <div className="container">
+            <div style={{ background: `url('${props.bgImg1}') no-repeat center center/cover` }} className="left"></div>
+            <div style={{ background: `url('${props.bgImg2}') no-repeat center center/cover` }} className="right"></div>
+        </div>
+    </div>
+    )
+}
+ 
+export default Food;
